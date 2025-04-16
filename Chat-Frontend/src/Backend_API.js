c89fa043b4f90e9581982ab971eca15c059c9966
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-04-16 14:27:32 +0530

    Update Backend_API.js

--- Chat-Frontend/src/Backend_API.js
+++ Chat-Frontend/src/Backend_API.js
@@ -1,1 +1,2 @@
-export const API = import.meta.env.VITE_BACKEND_URL;
+// export const API = import.meta.env.VITE_BACKEND_URL;
+export const API = "https://real-time-chat-application-1-awv3.onrender.com";
