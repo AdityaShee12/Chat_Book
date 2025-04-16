35c15c2cc61fa57dcc83caba3ce4766af3eeba1d
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-04-16 19:34:42 +0530

    Update vite.config.js and Backend_API.js file in Chat-Frontend file

--- Chat-Frontend/src/Backend_API.js
+++ Chat-Frontend/src/Backend_API.js
@@ -1,2 +1,1 @@
-// export const API = import.meta.env.VITE_BACKEND_URL;
-export const API = "https://real-time-chat-application-1-awv3.onrender.com";
+export const API = import.meta.env.VITE_BACKEND_URL;
