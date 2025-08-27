5313395f63fc4a7cce3a5cf0c169306f251c3f0c
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-08-27 07:41:55 +0530

    Wrap App component by Provider of react-redux

--- Chat-Frontend/src/main.jsx
+++ Chat-Frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { Router } from "react-router-dom";
-import "./App.css";
 import App from "./App.jsx";
+import "./App.css";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={App} />
+  <Provider store={store}>
+    <RouterProvider router={App} />
+  </Provider>
 );
