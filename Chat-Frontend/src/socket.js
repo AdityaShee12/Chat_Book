10358f8734f1c3e688f0d4def05f73eeadc23675
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-10-21 11:28:07 +0530

    Update backend api on socket.js file

--- Chat-Frontend/src/socket.js
+++ Chat-Frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://real-time-chat-application-klxp.onrender.com", {
+const socket = io("https://chat-book-u2yq.onrender.com", {
   withCredentials: true,
 });
 
