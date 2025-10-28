3af2107419f3f34b88ee63e2be2cafbde117e2f5
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-10-28 11:58:42 +0530

    Uninstall dotenv and use import.meta.env

--- Chat-Frontend/src/Backend_API.js
+++ Chat-Frontend/src/Backend_API.js
@@ -1,9 +1,5 @@
-// config.js
-import dotenv from "dotenv";
-dotenv.config();
-
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = import.meta.env.NODE === "production";
 
 export const BACKEND_API = isProduction
-  ? process.env.BACKEND_API_Production
-  : process.env.BACKEND_API_Localhost ;
+  ? import.meta.env.BACKEND_API_Production
+  : import.meta.env.BACKEND_API_Localhost;
