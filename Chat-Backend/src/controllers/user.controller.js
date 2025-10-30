f511590a8733a7d6de8c472c646272b4e83f69a6
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-10-30 14:15:13 +0530

    Change secure false from true on cookies code on user.controller.js file

--- Chat-Backend/src/controllers/user.controller.js
+++ Chat-Backend/src/controllers/user.controller.js
@@ -138,8 +138,8 @@ const loginUser = asyncHandler(async (req, res) => {
   );
   const options = {
     httpOnly: true,
-    secure: true,
-    sameSite: "None",
+    secure: false,
+    sameSite: "Lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   };
   return res
