be69736f15a6f3e52ae59c563f63e7c9909d4c5a
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2025-11-03 12:38:18 +0530

    Updated cloudinary config

--- Chat-Backend/src/utils/cloudinary.js
+++ Chat-Backend/src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: "drargfdfb",
+  api_key: "876421259555577",
+  api_secret: "kmHqL8MT1qDDCI8dL5Y79YfLo94",
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
